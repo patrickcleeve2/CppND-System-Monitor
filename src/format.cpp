fd411bac82ca5eb0074ac730e798e22bce3b0b0e
Patrick Cleeve <patrickcleeve2@gmail.com>
2021-11-11 16:26:44 +1100

    format changes

--- src/format.cpp
+++ src/format.cpp
@@ -1,19 +1,18 @@
-#include <string>
-
 #include "format.h"
+
+#include <string>
 
 using std::string;
 
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds) {   int hours, minutes;
+string Format::ElapsedTime(long seconds) {
+  // ref: https://stackoverflow.com/questions/25696992/converting-seconds-to-hours-and-minutes-and-seconds/52444808
+  int hours, minutes;
   minutes = seconds / 60;
   hours = minutes / 60;
 
   // elapsed time in HH:MM::SS
   return std::to_string(hours) + ":" + std::to_string(minutes % 60) + ":" +
-         std::to_string(seconds % 60); 
-         
+         std::to_string(seconds % 60);
 }
-// ref:
-// https://stackoverflow.com/questions/25696992/converting-seconds-to-hours-and-minutes-and-seconds/52444808
